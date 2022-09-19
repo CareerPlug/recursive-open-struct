919ea5feedf2f22b2b6e8029de0712619c1de3d0
CodeBuild <devteam@careerplug.com>
2022-09-19 12:20:15 -0400

    initialize with_init

--- lib/recursive_open_struct.rb
+++ lib/recursive_open_struct.rb
@@ -27,6 +27,24 @@ class RecursiveOpenStruct < OpenStruct
     }
   end
 
+  def init_with coder
+    if coder&.map&.present?
+      hash = coder.map["table"] || {}
+
+      @options = self.class.default_options.merge!({
+          mutate_input_hash: coder.map["mutate_input_hash"] || false,
+          recurse_over_arrays: coder.map["recurse_over_arrays"] || false,
+          preserve_original_keys: coder.map["preserve_original_keys"] || false
+        }).freeze
+
+      @deep_dup = DeepDup.new(@options)
+
+      @table = @options[:mutate_input_hash] ? hash : @deep_dup.call(hash)
+
+      @sub_elements = {}
+    end
+  end
+
   def initialize(hash=nil, passed_options={})
     hash ||= {}
 
@@ -103,7 +121,7 @@ class RecursiveOpenStruct < OpenStruct
   # Makes sure ROS responds as expected on #respond_to? and #method requests
   def respond_to_missing?(mid, include_private = false)
     mname = _get_key_from_table_(mid.to_s.chomp('=').chomp('_as_a_hash'))
-    @table&.key?(mname) || super
+    @table.key?(mname) || super
   end
 
   # Adapted implementation of method_missing to accommodate the differences
@@ -179,8 +197,8 @@ class RecursiveOpenStruct < OpenStruct
   end
 
   def _get_key_from_table_(name)
-    return name.to_s if @table&.has_key?(name.to_s)
-    return name.to_sym if @table&.has_key?(name.to_sym)
+    return name.to_s if @table.has_key?(name.to_s)
+    return name.to_sym if @table.has_key?(name.to_sym)
     name
   end
 
